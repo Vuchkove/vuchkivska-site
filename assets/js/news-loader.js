2eaeed17828bda3d970dafc0c7b48f45314ec8ce
Maksim Shpontak <maximalla1997@gmail.com>
2026-01-24 19:56:37 +0200

    turned on prettier

--- assets/js/news-loader.js
+++ assets/js/news-loader.js
@@ -1,32 +1,32 @@
-fetch('news.json')
-  .then(res => res.json())
-  .then(newsList => {
-    const newsContainer = document.getElementById('news-container');
-    newsContainer.innerHTML = '';
+fetch("news.json")
+	.then((res) => res.json())
+	.then((newsList) => {
+		const newsContainer = document.getElementById("news-container");
+		newsContainer.innerHTML = "";
 
-    newsList.forEach(item => {
-      const newsItem = document.createElement('div');
-      newsItem.className = 'news-item';
+		newsList.forEach((item) => {
+			const newsItem = document.createElement("div");
+			newsItem.className = "news-item";
 
-      // Картинки
-      let imagesHTML = '';
-      if (item.images && item.images.length) {
-        imagesHTML = '<div class="news-images">';
-        item.images.forEach(src => {
-          imagesHTML += `<img src="${src}" alt="Фото новини" class="news-img" />`;
-        });
-        imagesHTML += '</div>';
-      }
+			// Картинки
+			let imagesHTML = "";
+			if (item.images && item.images.length) {
+				imagesHTML = '<div class="news-images">';
+				item.images.forEach((src) => {
+					imagesHTML += `<img src="${src}" alt="Фото новини" class="news-img" />`;
+				});
+				imagesHTML += "</div>";
+			}
 
-      // Відео
-      let videoHTML = '';
-      if (item.video) {
-        videoHTML = `<div class="news-video">
+			// Відео
+			let videoHTML = "";
+			if (item.video) {
+				videoHTML = `<div class="news-video">
                        <iframe src="${item.video}" allowfullscreen></iframe>
                      </div>`;
-      }
+			}
 
-      newsItem.innerHTML = `
+			newsItem.innerHTML = `
         <h3>${item.title}</h3>
         <small>${item.date}</small>
         <p>${item.content}</p>
@@ -34,11 +34,11 @@ fetch('news.json')
         ${videoHTML}
       `;
 
-      newsContainer.appendChild(newsItem);
-    });
-  })
-  .catch(err => {
-    console.error('Помилка завантаження новин:', err);
-    const newsContainer = document.getElementById('news-container');
-    newsContainer.innerHTML = '<p>Не вдалося завантажити новини. Спробуйте пізніше.</p>';
-  });
+			newsContainer.appendChild(newsItem);
+		});
+	})
+	.catch((err) => {
+		console.error("Помилка завантаження новин:", err);
+		const newsContainer = document.getElementById("news-container");
+		newsContainer.innerHTML = "<p>Не вдалося завантажити новини. Спробуйте пізніше.</p>";
+	});
